e7e998e6dc1e5118b56969e6574d267beb215077
KLL <1247703485@qq.com>
2021-05-26 18:22:10 +0800

    [update] login page

--- src/app/pages/wallet/login/login.tsx
+++ src/app/pages/wallet/login/login.tsx
@@ -5,12 +5,20 @@ import useI18 from '../../../../i18n/hooks';
 
 import './login.scss';
 import { Link } from 'react-router-dom';
+import ComConSvg from '../../../components/control/icon';
 
 const PageWalletLogin: FC = () => {
   const textAreaPlaceholder = useI18('inputWordPlaceholder');
+  const passwordText = useI18('passwordTip');
   const [signInIng, setSignInIng] = useState(false);
   const [wordText, setWordText] = useState('');
   const [areaFocus, setAreaFocus] = useState(false);
+  const [inputType, setInputType] = useState<'password'|'text'>('password');
+  const [password, setPassword] = useState('');
+
+  const changeInputType = () => {
+    setInputType(status => status === 'text' ? 'password' : 'text');
+  };
 
   const login = () => {
     const words = wordText.split(/[^a-zA-Z]+/g).filter(item => Boolean(item));
@@ -30,6 +38,18 @@ const PageWalletLogin: FC = () => {
         onChange={e => setWordText(e.target.value)}
         onBlur={() => setAreaFocus(false)}
         onFocus={() => setAreaFocus(true)}></textarea>
+      <p className="wallet_password_tip_top"><I18 text="password" /><span className="wallet_password_important">*</span></p>
+      <div className="wallet_password_box">
+        <input
+          className="wallet_password_input"
+          placeholder={inputType === 'text' ? '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$/' : passwordText}
+          type={inputType}
+          value={password}
+          onChange={e => setPassword(e.target.value)} />
+        <button className="wallet_password_change" onClick={changeInputType}>
+          <ComConSvg className="wallet_password_icon" xlinkHref={inputType === 'text' ? '#icon-show' : '#icon-hide'} />
+        </button>
+      </div>
       <div className="wallet_buttons">
         <button
           className="wallet_button"
